eaeaf9f6315b8c37a95463c0581e94df24b00f08
Leon Shen <leon.g.shen0709@gmail.com>
2022-08-07 17:22:24 +0200

    feat: adds options filtering and sorting

--- components/MultiSelect/MultiSelect.tsx
+++ components/MultiSelect/MultiSelect.tsx
@@ -1,4 +1,9 @@
-import { FunctionComponent, InputHTMLAttributes } from "react";
+import React, {
+  FunctionComponent,
+  InputHTMLAttributes,
+  useMemo,
+  useState,
+} from "react";
 import styles from "./MultiSelect.module.scss";
 
 export interface MultiSelectOption {
@@ -19,31 +24,103 @@ const MultiSelect: FunctionComponent<MultiSelectProps> = ({
   loading,
   isErrored,
 }) => {
+  const [selectedOptions, setSelectedOption] = useState(new Set());
+  const [filterText, setFilterText] = useState("");
+
+  const handleFilterInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterText(event.target.value);
+  };
+
+  const filteredOptions = useMemo(() => {
+    if (!filterText)
+      return options.filter((option) => !selectedOptions.has(option));
+
+    return options.filter(
+      (option) =>
+        option.label.toLocaleLowerCase().match(filterText.toLowerCase()) &&
+        !selectedOptions.has(option)
+    );
+  }, [options, filterText, selectedOptions]);
+
+  const sortedSelectedOptions = useMemo(() => {
+    return (Array.from(selectedOptions) as Array<MultiSelectOption>).sort(
+      (a, b) => {
+        return a.label.localeCompare(b.label);
+      }
+    );
+  }, [selectedOptions]);
+
+  const handleCheckboxToggle = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    option: MultiSelectOption
+  ) => {
+    if (event.target.checked) {
+      selectedOptions.add(option);
+      setSelectedOption(new Set(selectedOptions));
+      return;
+    }
+
+    selectedOptions.delete(option);
+    setSelectedOption(new Set(selectedOptions));
+  };
+
+  const noAvailablesOptions =
+    filteredOptions.length === 0 && selectedOptions.size === 0;
+
   return (
     <div className={styles.multiselect}>
       <h3>{title}</h3>
+
+      <input
+        type="text"
+        onChange={handleFilterInput}
+        placeholder="Zoek op ..."
+      />
 
       <div className={styles.multiselect__options}>
         {loading ? (
           <p>Loading...</p>
         ) : isErrored ? (
           <p>Failed to get product groups</p>
+        ) : noAvailablesOptions ? (
+          <p>No product groups found.</p>
         ) : (
-          options.map((option, index) => {
-            const id = `${index} - ${option.label}`;
+          <>
+            {sortedSelectedOptions.map((option) => {
+              const id = `selected option - ${option.label}`;
 
-            return (
-              <div key={id}>
-                <input
-                  type="checkbox"
-                  id={id}
-                  name={option.label}
-                  value={option.value}
-                />
-                <label htmlFor={id}>{option.label}</label>
-              </div>
-            );
-          })
+              return (
+                <div key={id}>
+                  <input
+                    type="checkbox"
+                    id={id}
+                    name={option.label}
+                    value={option.value}
+                    onChange={(event) => handleCheckboxToggle(event, option)}
+                    checked
+                  />
+                  <label htmlFor={id}>{option.label}</label>
+                </div>
+              );
+            })}
+
+            {filteredOptions.map((option) => {
+              const id = `filtered option - ${option.label}`;
+
+              return (
+                <div key={id}>
+                  <input
+                    type="checkbox"
+                    id={id}
+                    name={option.label}
+                    value={option.value}
+                    onChange={(event) => handleCheckboxToggle(event, option)}
+                  />
+                  <label htmlFor={id}>{option.label}</label>
+                </div>
+              );
+            })}
+          </>
         )}
       </div>
     </div>
