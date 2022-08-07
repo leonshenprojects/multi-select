fcdc22cee13351542791bf1373a52b8241aa6020
Leon Shen <leon.g.shen0709@gmail.com>
2022-08-07 11:43:20 +0200

    feat(api): creates endpoint that returns product groups

---
+++ pages/api/productGroups.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import productGroupData from "../../lib/productGroupData";
+
+type ProductGroupData = {
+  productGroups: Array<string>;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ProductGroupData>
+) {
+  if (req.method === "GET") {
+    return res.status(200).json({ productGroups: productGroupData.data });
+  }
+
+  return res.status(405);
+}
